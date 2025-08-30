f2b6fcd740a64522ae61fdf2e84307b8b36b584d
Vansh Singh <167158523+Vansh3829@users.noreply.github.com>
2025-08-31 00:59:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,4 +77,5 @@ def upload_and_process():
     return render_template('index.html', extracted_text=extracted_text, analysis_results=results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
+
